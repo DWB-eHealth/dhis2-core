8f868306289a4f9635b210adfb74d0f55d1c5e99
Rajashri <jadhavrajashri49@gmail.com>
2018-02-21 15:31:25 +0530

    Revert "response message if period is already approved"
    This reverts commit 3c8c84bb157eb9848b644115354f89c9bc20a707.

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/sms/listener/DataValueSMSListener.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/sms/listener/DataValueSMSListener.java
@@ -81,8 +81,6 @@ public class DataValueSMSListener
 {
     private static final String defaultPattern = "([a-zA-Z]+)\\s*(\\d+)";
 
-    private static final String DATASET_LOCKED = "Dataset: %s is locked for period: %s";
-
     // -------------------------------------------------------------------------
     // Dependencies
     // -------------------------------------------------------------------------
@@ -153,13 +151,10 @@ public class DataValueSMSListener
         OrganisationUnit orgUnit = SmsUtils.selectOrganisationUnit( orgUnits, parsedMessage, smsCommand );
         Period period = getPeriod( smsCommand, date );
 
-        DataSet dataSet = smsCommand.getDataset();
-
-        if ( dataSetService.isLocked( dataSet, period, orgUnit, dataElementCategoryService.getDefaultDataElementCategoryOptionCombo(), null ) )
+        if ( dataSetService.isLocked( smsCommand.getDataset(), period, orgUnit, null, null ) )
         {
-            smsSender.sendMessage( null, String.format( DATASET_LOCKED, dataSet.getUid(), period.getName() ), sms.getOriginator() );
-
-            throw new SMSParserException( String.format( DATASET_LOCKED, dataSet.getUid(), period.getName() ) );
+            throw new SMSParserException(
+                "Dataset is locked for the period " + period.getStartDate() + " - " + period.getEndDate() );
         }
 
         boolean valueStored = false;
